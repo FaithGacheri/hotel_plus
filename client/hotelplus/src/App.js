fc67571e9342a737bc235fca7bba4db023de5457
“[firstname <faith.gacheri@student.moringaschool.com>
2022-11-07 13:01:40 +0300

    setting up the environment

---
+++ client/hotelplus/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div id='container'>
+       <h1>Hotel plus</h1>
+       </div>
+    </div>
+  );
+}
+
+export default App;
