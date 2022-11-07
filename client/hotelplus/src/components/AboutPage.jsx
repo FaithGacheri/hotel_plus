b89e56caef9f85e6a241c93e9b25409e7310267f
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-11-07 20:52:09 +0300

    adding tag fields

--- client/hotelplus/src/components/AboutPage.jsx
+++ client/hotelplus/src/components/AboutPage.jsx
@@ -4,6 +4,15 @@ function AboutPage() {
 return (
 <div className='about-page'>
 
+<h2 id='about-heading'>Why You Should Choose Us.</h2>
+<p id='about-parags'>You Should choose us because we provide the best accomodation and we have sorted all the hotels<br />here based on their quality </p>
+<button id = 'one'>01</button>
+<p></p>
+<button id = 'one'>02</button>
+<p></p>
+<button id = 'one'>03</button>
+<p></p>
+
 
 </div>
 )
