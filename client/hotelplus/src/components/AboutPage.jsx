0edf1e7cff234fc43029d04d64d14a1b1b469de5
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-11-07 22:04:32 +0300

    Adding hotel image

--- client/hotelplus/src/components/AboutPage.jsx
+++ client/hotelplus/src/components/AboutPage.jsx
@@ -6,13 +6,13 @@ return (
 
 <h2 id='about-heading'>Why You Should Choose Us.</h2>
 <p id='about-parags'>You Should choose us because we provide the best accomodation and we have sorted all the hotels<br />here based on their quality </p>
-<button id = 'one'>01</button>
-<p></p>
-<button id = 'one'>02</button>
-<p></p>
-<button id = 'one'>03</button>
-<p></p>
-
+<button id = 'zero-all'>01</button>
+<p id='about-parags'>We always prioritize customer comfort and<br />satisfaction. That's why we only accept the best hotels</p>
+<button id = 'zero-all'>02</button>
+<p id='about-parags'>Although the price tends to be cheaper but it will not<br />affect the quality of service, so don't worry</p>
+<button id = 'zero-all'>03</button>
+<p id='about-parags'>Don't worry if suddenly you have a problem and want<br />to do a refund, with us you can get a full refund</p>
+<img src="https://i.ibb.co/ccNzfr1/Hotel-Image.jpg" alt='hotel' id='about-hotel-img'></img>
 
 </div>
 )
