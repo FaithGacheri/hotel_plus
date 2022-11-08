f32b985cd84e5a811aa6e575aa1e7ea0a7514db2
Emanukim <emmanue.kimweli@student.moringaschool.com>
2022-11-08 12:48:54 +0300

    add room css and react

--- client/hotelplus/src/components/HomePage.jsx
+++ client/hotelplus/src/components/HomePage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Rooms from './Rooms'
 
 function HomePage() {
   return (
     <div>
-      <h1>Hello World</h1>
         {/* This is our home page */}
         {/* Denis Compontent */}
 
         {/* Ian Component */}
 
         {/* Imanuels Component */}
+        <Rooms />
 
         {/* Joseph Component */}
 
---
+++ client/hotelplus/src/components/Rooms.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import Room1 from '../components/image/room1.jpg'
+import Room2 from '../components/image/room2.jpg'
+import Room3 from '../components/image/room3.jpg'
+import Room4 from '../components/image/room4.jpg'
+import Room5 from '../components/image/room5.jpg'
+import Room6 from '../components/image/room6.jpg'
+import './Room.css'
+
+
+function Rooms() {
+  return (
+    <div className='rooms'>
+      <h1>The Best Hotel Rooms for you</h1>
+      <div className='p' >This are some of the hotels we highly recommend for you. We guarantee the quality of the services, the food, the hotel area and various other aspects.</div >
+      
+      <div id='roomContainer'>
+        <div className='room'>
+          <img src={Room1} alt="Room1" />
+          <h5>Java Heritage Hotel Room</h5>
+          <p>starts from ksh 15,000/night</p>
+          <button>Book Now</button>
+        </div>
+
+        <div className='room'>
+          <img src={Room2} alt="Room2" />
+          <h5>Grand Aston Hotel Room</h5>
+          <p>starts from Ksh 25,000/night</p>
+          <button>Book Now</button>
+        </div>
+        
+        <div className='room'>
+          <img src={Room3} alt="Room3" />
+          <h5>Nihi Sumba Resort Room</h5>
+          <p>starts from Ksh 22,000/night</p>
+          <button>Book Now</button>
+          </div>
+          </div>
+
+          <div id='roomContainer'>
+
+        <div className='room'>
+          <img src={Room4} alt="Room4" />
+          <h5>Yellowstar Hotel Jimbaran Room</h5>
+          <p>starts from Ksh 17,000/night</p>
+          <button>Book Now</button>
+        </div>
+        
+        <div className='room'>
+          <img src={Room5} alt="Room5" />
+          <h5>The Alan Hotel Room</h5>
+          <p>starts from Ksh 14,000/night</p>
+          <button>Book Now</button>
+        </div>
+        
+        <div className='room'>
+          <img src={Room6} alt="Room6" />
+          <h5>Masai Mara Hotel Room</h5>
+          <p>starts from Ksh 20,000/night</p>
+          <button>Book Now</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Rooms
