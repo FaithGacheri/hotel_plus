ec03e6df122aa1b03d1f20ef3620695c98713a59
DenisOuma <dennisouma38@gmail.com>
2022-11-09 22:01:56 +0300

    Updting the user data from components modules

--- client/hotelplus/src/App.js
+++ client/hotelplus/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import HomePage from "./components/HomePage"
+import "./App.css";
+import HomePage from "./components/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WelcomeSection from "./components/WelcomeSection";
 function App() {
 	return (
 		<>
 			<Router>
 				<div className="App">
 					<div id="container">
+						<WelcomeSection />
 						<Routes>
 							<Route path="/" element={<HomePage />} />
 						</Routes>
@@ -15,8 +17,6 @@ function App() {
 			</Router>
 		</>
 	);
-
-
 }
 
 export default App;
--- client/hotelplus/src/components/HomePage.jsx
+++ client/hotelplus/src/components/HomePage.jsx
@@ -8,7 +8,6 @@ import Footer from "./Footer";
 function HomePage() {
 	return (
 		<div>
-			<WelcomeSection />
 			<AboutPage />
 			<Rooms />
 			<Slider />
