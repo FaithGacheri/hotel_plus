45936d02c5bb4c5c09f4889775ac2b2a91314d71
“[firstname <faith.gacheri@student.moringaschool.com>
2022-11-10 09:09:51 +0300

    edit alt message

--- client/hotelplus/src/components/Room.jsx
+++ client/hotelplus/src/components/Room.jsx
@@ -5,7 +5,7 @@ import '../styles/Room.css'
 function Room({id,image,description,perNightPrice}) {
   return (
     <div className='room' key={id} >
-        <img src={image} alt="room image" />
+        <img src={image} alt="room" />
         <h5>{description}</h5>
         <p>starts from Ksh {perNightPrice}/night</p>
         <button>Book Now</button>
