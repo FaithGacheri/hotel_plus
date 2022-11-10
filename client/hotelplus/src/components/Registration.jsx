bf0689efa14df0d1b92c69eb5d5a694d2f1a2a36
DenisOuma <dennisouma38@gmail.com>
2022-11-10 06:27:57 +0300

    User able to register | user signup data svaed to state

--- client/hotelplus/src/components/Registration.jsx
+++ client/hotelplus/src/components/Registration.jsx
@@ -2,9 +2,44 @@ import React from "react";
 import "../styles/Registration.css";
 
 function Registration() {
+	const [user, setUser] = React.useState({
+		fullName: "",
+		userName: "",
+		email: "",
+		phoneNumber: "",
+		password: "",
+		confirmPassword: "",
+		gender: "",
+	});
+
+	const onChange = (e) => {
+		setUser({ ...user, [e.target.name]: e.target.value });
+	};
+
+	const registerUser = (e) => {
+		e.preventDefault();
+		const userAccount = {
+			fullName: user.fullName,
+			userName: user.userName,
+			email: user.email,
+			phoneNumber: user.phoneNumber,
+			password: user.password,
+			gender: user.gender,
+		};
+		setUser({
+			fullName: "",
+			userName: "",
+			email: "",
+			phoneNumber: "",
+			password: "",
+			confirmPassword: "",
+		});
+		console.log(userAccount);
+	};
+
 	return (
 		<div class="box" id="r-box">
-			<form autocomplete="off" id="my-login-form">
+			<form autocomplete="off" id="my-login-form" onSubmit={registerUser}>
 				<h2 id="h2-login-form">Sign Up to HotelPlus</h2>
 				<div class="main-user-info">
 					<div class="inputBox">
@@ -12,9 +47,9 @@ function Registration() {
 							required="required"
 							id="login-input"
 							type="text"
-							name="email"
-							// value={user.email}
-							// onChange={onChange}
+							name="fullName"
+							value={user.fullName}
+							onChange={onChange}
 						></input>
 						<span>Full Name</span>
 						<i></i>
@@ -24,9 +59,9 @@ function Registration() {
 							required="required"
 							id="login-input"
 							type="text"
-							name="email"
-							// value={user.email}
-							// onChange={onChange}
+							name="userName"
+							value={user.userName}
+							onChange={onChange}
 						></input>
 						<span>Enter User Name</span>
 						<i></i>
@@ -37,8 +72,8 @@ function Registration() {
 							id="login-input"
 							type="text"
 							name="email"
-							// value={user.email}
-							// onChange={onChange}
+							value={user.email}
+							onChange={onChange}
 						></input>
 						<span>Enter Email</span>
 						<i></i>
@@ -48,9 +83,9 @@ function Registration() {
 							required="required"
 							id="login-input"
 							type="text"
-							name="email"
-							// value={user.email}
-							// onChange={onChange}
+							name="phoneNumber"
+							value={user.phoneNumber}
+							onChange={onChange}
 						></input>
 						<span>Phone Number</span>
 						<i></i>
@@ -62,8 +97,8 @@ function Registration() {
 							required="required"
 							id="login-input"
 							name="password"
-							// value={user.password}
-							// onChange={onChange}
+							value={user.password}
+							onChange={onChange}
 						></input>
 						<span>Password</span>
 						<i></i>
@@ -74,9 +109,9 @@ function Registration() {
 							type="password"
 							required="required"
 							id="login-input"
-							name="password"
-							// value={user.password}
-							// onChange={onChange}
+							name="confirmPassword"
+							value={user.confirmPassword}
+							onChange={onChange}
 						></input>
 						<span>Confirm Password</span>
 						<i></i>
@@ -86,11 +121,29 @@ function Registration() {
 					<h2 id="h2-login-formr">Gender</h2>
 
 					<div class="gender-category">
-						<input type="radio" name="gender" id="male" />
+						<input
+							type="radio"
+							name="male"
+							id="male"
+							value={user.gender}
+							onChange={onChange}
+						/>
 						<label for="male">Male</label>
-						<input type="radio" name="gender" id="female" />
+						<input
+							type="radio"
+							name="female"
+							id="female"
+							value={user.gender}
+							onChange={onChange}
+						/>
 						<label for="female">Female</label>
-						<input type="radio" name="gender" id="other" />
+						<input
+							type="radio"
+							name="other"
+							id="other"
+							value={user.gender}
+							onChange={onChange}
+						/>
 						<label for="other">Other</label>
 					</div>
 				</div>
